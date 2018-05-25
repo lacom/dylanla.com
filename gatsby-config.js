4afa499fdce3f3057f039585fa1d2831958af0a7
Dylan La Com <dylanlacom@dylan.local>
2018-05-25 10:42:36 -0700

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -174,7 +174,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-80332066-1',
       },
     },
     {
